1a04818c49bc7e51d7d620ba28dca4b40a20358d
crist138 <cristoballongares@gmail.com>
2022-09-18 11:50:25 -0500

    Added Porygon

---
+++ pokemones/Porygon.java
@@ -0,0 +1,9 @@
+package pokemones;
+import java.util.*;
+
+public class Porygon extends Pokemon{
+
+	public Porygon(String nombre, int tipo, int vida, String[] ataques){
+		super(nombre,tipo,vida,ataques);
+	}
+}
